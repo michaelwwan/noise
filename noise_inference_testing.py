56566aea43d2f6907672fbf0773cb48272c1e0e9
Ryan Neilson <160774919+ryanneilson-neu@users.noreply.github.com>
2026-02-11 11:35:41 -0500

    Delete noise_inference_testing.py

--- noise_inference_testing.py
+++
@@ -1,9 +0,0 @@
-from noise_inference import percent_ocl_area_per_well
-
-def testing():
-
-    print(percent_ocl_area_per_well(50,100))
-
-testing()
-
-# From the CLI, use python noise_inference_testing.py
